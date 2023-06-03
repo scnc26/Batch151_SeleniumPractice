91d12ad98cacfd72dc549ad3f8030c94d3981ef2
Murat <scnc2626@gmail.com>
2023-06-03 20:19:24 +0300

    03062023

---
+++ src/test/java/Day01_practice/C02.java
@@ -0,0 +1,56 @@
+package Day01_practice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.List;
+
+public class C02 {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver","src/resources/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+
+        // https://www.amazon.com/ adresine gidin
+        driver.get("https://www.amazon.com/");
+
+
+
+
+        // arama motorunda nutella yazip aratınız
+        driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']")).sendKeys("Nutella" + Keys.ENTER);
+
+
+
+
+        // Customer Service ve Registry butonlarına sırasıyla tıklayın ve title'larının "Amazon" icerdigini test edin
+
+
+        for (int i = 0; i <2 ; i++) {
+
+            List<WebElement> butonlar = driver.findElements(By.xpath("(//a[@tabindex='0'])[6] | (//a[@tabindex='0'])[7]"));
+            butonlar.get(i).click();
+
+            if(driver.getTitle().contains("Amazon")){
+                System.out.println("Title Amazon Iceriyor");
+            }
+
+            driver.navigate().back();
+
+        }
+
+
+
+
+        // pencereyi kapatiniz
+        driver.close();
+
+}}
